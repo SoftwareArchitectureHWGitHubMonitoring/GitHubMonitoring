f39fa8d88c565ab0d8e7a8bdbee92304451f6c29
barabali <balintbarath@gmail.com>
2018-11-07 20:21:49 +0100

    Initial commit

---
+++ GitMonitor/GitMonitorHome.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitMonitor
+{
+    /// <summary>
+    /// Interaction logic for GitMonitorHome.xaml
+    /// </summary>
+    public partial class GitMonitorHome : Page
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public GitMonitorHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            GitMonitorReportPage expenseReportPage = new GitMonitorReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
